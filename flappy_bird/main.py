982226bead0f1d28b394e70b4eef6610679fded7
GVatest <66703210+GVatest@users.noreply.github.com>
2021-01-07 15:43:45 +1000

    pipes complete

--- flappy_bird/main.py
+++ flappy_bird/main.py
@@ -1,9 +1,28 @@
 import pygame
 import sys
 import os
+import random
 
 SCREEN_SIZE = (360, 640)
 BLACK = (0, 0, 0)
+
+
+def create_pipe():
+    new_pipe = pipe.get_rect(top=random.choice([i for i in range(200, 480)]), left=360)
+    return new_pipe, pipe.get_rect(bottom=new_pipe.top - 150, left=360)
+
+
+def move_pipes(pipes):
+    for p in pipes:
+        p[0].centerx -= 4
+        p[1].centerx -= 4
+    return pipes
+
+
+def draw_pipes(pipes):
+    for p in pipes:
+        screen.blit(pipe, p[0])
+        screen.blit(pipe_rotated, p[1])
 
 
 def load_image(name, colorkey=None):
@@ -42,13 +61,12 @@ ground_rect.top = SCREEN_SIZE[1] - ground_rect.bottom
 
 # Bird #################
 bird = load_image('bird.png', -1)
-bird = pygame.transform.scale(bird, (60, 50))
-bird_rect = bird.get_rect(center=(100, 512))
-bird_rect.top = SCREEN_SIZE[1] // 2
+bird = pygame.transform.scale(bird, (50, 40))
+bird_rect = bird.get_rect(center=(100, 320))
 bird_flappy = load_image('bird_flapp.png')
-bird_flappy = pygame.transform.scale(bird_flappy, (60, 50))
+bird_flappy = pygame.transform.scale(bird_flappy, (50, 40))
 bird_fly = load_image('bird_fly.png')
-bird_fly = pygame.transform.scale(bird_fly, (60, 50))
+bird_fly = pygame.transform.scale(bird_fly, (50, 40))
 
 # Bird Animation #################
 FLAPPEVENT = pygame.USEREVENT
@@ -58,11 +76,14 @@ gravity = 0
 bird_movement = 0
 
 # Pipes #################
-pipe = load_image('pipe.png')
+pipe = load_image('pipe.png', -1)
+pipe = pygame.transform.scale(pipe, (50, 500))
+pipe_rotated = pygame.transform.rotate(pipe, 180)
 
 
-
-
+PIPESPAWNEVENT = pygame.USEREVENT + 1
+pygame.time.set_timer(PIPESPAWNEVENT, 1500)
+pipes_group = []
 
 
 while 1:
@@ -70,23 +91,30 @@ while 1:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == pygame.USEREVENT:
+        if event.type == FLAPPEVENT:
             flapp_counter = (flapp_counter + 1) % 3
-
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 gravity = 0.25
                 bird_movement = 0
                 bird_movement -= 9
+        if event.type == PIPESPAWNEVENT:
+            pipes_group.append(create_pipe())
 
     screen.fill(BLACK)
 
+    screen.blit(bg, (0, 0))
+
+    # pipes
+    pipes_group = move_pipes(pipes_group)
+    draw_pipes(pipes_group)
+
+
     # ground movement #################
-    ground_rect.left -= 2
+    ground_rect.left -= 4
     if ground_rect.right <= SCREEN_SIZE[0]:
         ground_rect.left = 0
 
-    screen.blit(bg, (0, 0))
     screen.blit(ground, ground_rect)
 
     # bird movement #################
